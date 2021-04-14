bde49df394c5b5d373b6277f344ae258cf0f8208
Emiliano Rago <emiliano.rg93@gmail.com>
2021-04-14 01:35:50 -0300

    implement lazy loading in app-routing.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NoPageFoundComponent } from './core/no-page-found/no-page-found.component';
-import { PagesRoutingModule } from './pages/pages-routing.module';
+
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  {
+    path: '',
+    loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule)
+  },
   { path: '**', component: NoPageFoundComponent },
 ];
 
 @NgModule({
   imports: [
     RouterModule.forRoot(routes),
-    PagesRoutingModule
   ],
   exports: [RouterModule]
 })
